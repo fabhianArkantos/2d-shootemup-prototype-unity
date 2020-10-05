96a5f34e01c2c58ccc684b0f9139f710e1bdb955
Víctor Fabián Villar Vergara <fabhian.villar@gmail.com>
2020-10-05 19:37:49 -0300

    Create project, movement feature, and camera Boundaries

---
+++ 2d-shootemup-prototype/Assets/Scripts/CameraBoundaries.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBoundaries : MonoBehaviour
+{
+    /**
+     * Code created by Press Start, a great GameDev Youtube Channel.
+     *  
+     */
+
+    #region Components
+    [SerializeField]
+    private Camera MainCamera;
+    #endregion
+
+    #region Variables
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    [SerializeField]
+    private bool isOrthographic = true;
+    #endregion
+
+    // Use this for initialization
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (isOrthographic)
+        {
+            CalculateBoundariesOrthographic();
+        }
+        else
+        {
+            CalculateBoundariesPerspective();
+        }
+    }
+
+    private void CalculateBoundariesPerspective()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
+        transform.position = viewPos;
+    }
+
+    private void CalculateBoundariesOrthographic()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
---
+++ 2d-shootemup-prototype/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Physics
+    [SerializeField]
+    //[Range(5f, 15f)]
+    private float speed = 10f;
+    #endregion
+
+    #region Variables
+    private Vector2 movement;
+    #endregion
+
+    #region Components
+    private Rigidbody2D rb2d;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition((Vector2) transform.position + movement * speed * Time.deltaTime);
+        
+    }
+}
